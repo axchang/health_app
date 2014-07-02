9974245a8eee44aff10e951ff5a8c452f5cf59c2
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-02 17:26:18 -0400

    first attempts at user build

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,3 +1,3 @@
-class Specialty < ActiveRecord::Base
+	class Specialty < ActiveRecord::Base
 	belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+
+resources :users, :only => [:index, :create, :new, :destroy]
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+root 'requests#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
