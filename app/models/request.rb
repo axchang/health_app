6fa23c17ca8c5a10626c078b944b9e6dad137004
Amy Chang <amy.chang@yale.edu>
2014-07-06 15:30:01 +0200

    email will send to last user

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,16 @@
 class Request < ActiveRecord::Base
 	belongs_to :need
 	belongs_to :user
+
+	#Call back to trigger email send after the request is actually created
+	after_create :send_request_posted_email
+	# Validate that certain parameters exist before the email is sent--DOUBLE CHECK
+	# validates 
+
+	protected
+
+	def send_request_posted_email
+		UserMailer.request_posted_email(self).deliver
+	end
+	
 end
