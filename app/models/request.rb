b62a333496310d371ea82bf43c059660bf743623
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-08 17:31:02 -0400

    commented message portion of request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,16 +9,16 @@ class Request < ActiveRecord::Base
 
 	protected
 
-	def send_request_posted_email
-		#Gets the need_id corresponding to the request that was just created
-	    iteration_id = self.need_id
-	    #Creates an array of all of the users with the given optin
-	    recipients = Need.find(iteration_id).users
-	    #Creates an array of all of the emails of the users with the given optin
-	    to_email = recipients.pluck(:email)
-	    #Actually delivers to each of the emails in the array
-	    to_email.each do |email|
-			UserMailer.request_posted_email(self, email).deliver
-		end
-	end
+	# def send_request_posted_email
+	# 	#Gets the need_id corresponding to the request that was just created
+	#     iteration_id = self.need_id
+	#     #Creates an array of all of the users with the given optin
+	#     recipients = Need.find(iteration_id).users
+	#     #Creates an array of all of the emails of the users with the given optin
+	#     to_email = recipients.pluck(:email)
+	#     #Actually delivers to each of the emails in the array
+	#     to_email.each do |email|
+	# 		UserMailer.request_posted_email(self, email).deliver
+	# 	end
+	# end
 end
