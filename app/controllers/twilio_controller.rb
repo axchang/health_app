04fd227efcec136b850aa02ac2dff2af4bf0bb0c
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-08 17:59:36 -0400

    put in dummy string for user in twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,7 +3,7 @@ class TwilioController < ApplicationController
   def process_sms
     @city = params[:FromCity].capitalize
     @state = params[:FromState]
-    @request = Request.create(description: params[:Body])
+    @request = Request.create(description: params[:Body], user: "hip-hop anonymous")
 
     #render 'process_sms.xml.erb', :content_type => 'text/xml'
   end
