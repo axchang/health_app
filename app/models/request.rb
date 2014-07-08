339c425c14faaacbc51932a7a76d39b28bfa7a8f
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-08 17:35:02 -0400

    seeded

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,7 @@ class Request < ActiveRecord::Base
 	belongs_to :user
 
 	#Call back to trigger email send after the request is actually created
-	after_create :send_request_posted_email
+	#after_create :send_request_posted_email
 	# Validate that certain parameters exist before the email is sent--DOUBLE CHECK
 	# validates 
 
