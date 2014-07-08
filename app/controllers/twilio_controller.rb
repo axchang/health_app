be13449c12fae110ee1886bfb6c2afc415f89668
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-08 18:19:01 -0400

    fixed user_id

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,7 +3,8 @@ class TwilioController < ApplicationController
   def process_sms
     @city = params[:FromCity].capitalize
     @state = params[:FromState]
-    @request = Request.create(description: params[:Body], user: "hip-hop anonymous")
+    @request = Request.create(description: params[:Body], user_id: 1)
+    #how do we set a user - can we seed this?
 
     #render 'process_sms.xml.erb', :content_type => 'text/xml'
   end
