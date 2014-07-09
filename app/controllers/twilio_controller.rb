e438cd99190b90dab1c1f6a086013420c92766ea
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-09 13:21:56 -0400

    added back #2

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -13,9 +13,9 @@ class TwilioController < ApplicationController
 	  
 		  if session["counter"] == 0
 			render 'first_message.xml.erb', :content_type => 'text/xml'	    
-		  # elsif session["counter"] == 1
-		  #    @request = Request.create(need_id: params[:Body], user_id: 1)
-		  #    render 'second_message.xml.erb', :content_type => 'text/xml'
+		  elsif session["counter"] == 1   
+		  	render 'second_message.xml.erb', :content_type => 'text/xml'
+		  	@request = Request.create(need_id: params[:Body], user_id: 1)
 		  # elsif session["counter"] == 2
 		  # #this may not be ideal if someone adds a request on the web during the SMS interaction
 		  #    @request.update(description: params[:Body])
