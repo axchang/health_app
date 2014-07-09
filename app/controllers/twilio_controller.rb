eb5b0fc243e517946b4043c5f8c2b1981455ee10
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-09 10:45:59 -0400

    sms sessions 45

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -10,19 +10,18 @@ class TwilioController < ApplicationController
   # end
 
 	def process_sms
+	  process do 
 	  session["counter"] ||= 0
 	  sms_count = session["counter"]
 		  if sms_count == 0
 		    message = "Thanks for contacting Insight Health! What kind of request are you making? Reply with 1 for Medication, 2 for Vaccination, 3 for Other."
-		  elsif sms_count == 1
+		  else sms_count == 1
 		    @request = Request.create(user_id: 1, need_id: params[:Body])
 		    message = "Got it! You can reply with a description, otherwise we'll post your request now!"
-		  else
-		    #@request = Request.create(description: params[:Body], user_id: 1, need_id: params[:Body])
-		    message = "Okay, I'm still figuring this part out..."
 		  end
 	  twiml = Twilio::TwiML::Response.new do |r|
 	    r.Sms message
+	  end
 	  session["counter"] += 1
 	  twiml.text
 	end
