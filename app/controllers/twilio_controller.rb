a443ddaedb6655cc9d79adf57ca39b030f207391
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-09 11:20:34 -0400

    changed message to views

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -12,16 +12,13 @@ class TwilioController < ApplicationController
 	  session["counter"] ||= 0
 	  sms_count = session["counter"]
 		  if sms_count == 0
-		    message = "Thanks for contacting Insight Health! What kind of request are you making? Reply with 1 for Medication, 2 for Vaccination, 3 for Other."
+			render 'first_message.xml.erb', :content_type => 'text/xml'	    
 		  else sms_count == 1
 		    @request = Request.create(user_id: 1, need_id: params[:Body])
-		    message = "Got it! You can reply with a description, otherwise we'll post your request now!"
+		    render 'second_message.xml.erb', :content_type => 'text/xml'
 		  end
-	  twiml = Twilio::TwiML::Response.new do |r|
-	    r.Sms message
-	  end
 	  session["counter"] += 1
-	  twiml.text
+	  #twiml.text
 	end
 
 end
