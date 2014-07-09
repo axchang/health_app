307ddbaea56471fbbfd883bb2f2dadcbe54538ae
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-09 12:48:22 -0400

    retrying sms

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -11,19 +11,19 @@ class TwilioController < ApplicationController
 	def process_sms
 	  session["counter"] ||= 0
 	  sms_count = session["counter"]
-		  #if sms_count == 0
+		  if sms_count == 0
 			render 'first_message.xml.erb', :content_type => 'text/xml'	    
-		  # elsif sms_count == 1
-		  #   @request = Request.create(need_id: params[:Body], user_id: 1)
-		  #   render 'second_message.xml.erb', :content_type => 'text/xml'
-		  # elsif sms_count == 2
-		  #  	#this may not be ideal if someone adds a request on the web during the SMS interaction
-		  #  	@request.update(description: params[:Body])
-		  #   render 'third_message.xml.erb', :content_type => 'text/xml'
-		  #   session["counter"] = -1
-		  # else
-		  # 	session["counter"] = -1
-		  # end
+		  elsif sms_count == 1
+		     @request = Request.create(need_id: params[:Body], user_id: 1)
+		     render 'second_message.xml.erb', :content_type => 'text/xml'
+		  elsif sms_count == 2
+		  #this may not be ideal if someone adds a request on the web during the SMS interaction
+		    	@request.update(description: params[:Body])
+		     render 'third_message.xml.erb', :content_type => 'text/xml'
+		     session["counter"] = -1
+		   else
+		   	session["counter"] = -1
+		   end
 	  session["counter"] += 1
 	  #twiml.text
 	end
