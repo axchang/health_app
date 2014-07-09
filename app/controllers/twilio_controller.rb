cb976bc5f548193946cf4d787eda3d0de1c5296b
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-09 10:57:22 -0400

    amys genius idea

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -9,21 +9,21 @@ class TwilioController < ApplicationController
   #   #render 'process_sms.xml.erb', :content_type => 'text/xml'
   # end
 
-	# def process_sms
-	#   process do 
-	#   session["counter"] ||= 0
-	#   sms_count = session["counter"]
-	# 	  if sms_count == 0
-	# 	    message = "Thanks for contacting Insight Health! What kind of request are you making? Reply with 1 for Medication, 2 for Vaccination, 3 for Other."
-	# 	  else sms_count == 1
-	# 	    @request = Request.create(user_id: 1, need_id: params[:Body])
-	# 	    message = "Got it! You can reply with a description, otherwise we'll post your request now!"
-	# 	  end
-	#   twiml = Twilio::TwiML::Response.new do |r|
-	#     r.Sms message
-	#   end
-	#   session["counter"] += 1
-	#   twiml.text
-	# end
+	def process_sms
+	  while true 
+	  session["counter"] ||= 0
+	  sms_count = session["counter"]
+		  if sms_count == 0
+		    message = "Thanks for contacting Insight Health! What kind of request are you making? Reply with 1 for Medication, 2 for Vaccination, 3 for Other."
+		  else sms_count == 1
+		    @request = Request.create(user_id: 1, need_id: params[:Body])
+		    message = "Got it! You can reply with a description, otherwise we'll post your request now!"
+		  end
+	  twiml = Twilio::TwiML::Response.new do |r|
+	    r.Sms message
+	  end
+	  session["counter"] += 1
+	  twiml.text
+	end
 
 end
