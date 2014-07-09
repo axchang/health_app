0336b584acf629de41b09fd973266a793e503d7e
Mike Cacchio <michael.cacchio@gmail.com>
2014-07-09 11:53:23 -0400

    trying for third aspect, ended session end

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -13,9 +13,14 @@ class TwilioController < ApplicationController
 	  sms_count = session["counter"]
 		  if sms_count == 0
 			render 'first_message.xml.erb', :content_type => 'text/xml'	    
-		  else sms_count == 1
+		  elsif sms_count == 1
 		    @request = Request.create(user_id: 1, need_id: params[:Body])
 		    render 'second_message.xml.erb', :content_type => 'text/xml'
+		   else
+		   	#this may not be ideal if someone adds a request on the web during the SMS interaction
+		   	@request.update(description: params[:Body])
+		    render 'third_message.xml.erb', :content_type => 'text/xml'
+		    session["counter"] = -1
 		  end
 	  session["counter"] += 1
 	  #twiml.text
